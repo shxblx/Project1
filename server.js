91e4a2006cda132d9e330dc336c431ddf4e3c32d
Muhammed Shibli A C <shiblibasheer27@gmail.com>
2025-04-19 14:13:27 +0530

    render sleep fix

--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const session = require('express-session');
-const flash = require('express-flash');
-const path = require('path');
-require('dotenv').config();
-const morgan=require('morgan')
+const mongoose = require("mongoose");
+const express = require("express");
+const session = require("express-session");
+const flash = require("express-flash");
+const path = require("path");
+require("dotenv").config();
+const morgan = require("morgan");
 
 const app = express();
 
-
-
-
-
-app.use(express.static(path.resolve(__dirname, 'public')));
-app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(__dirname, "public")));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: 'shibli', 
+app.use(
+  session({
+    secret: "shibli",
     resave: true,
     saveUninitialized: true,
-}));
+  })
+);
 
 app.use(flash());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-const userRouter = require('./routes/userRouter');
-app.use('/', userRouter);
-const adminRouter=require('./routes/adminRouter');
-app.use('/admin',adminRouter)
-const wishlistRoute=require('./routes/wishlistRouter');
-const { changePassword } = require('./controller/user_controller');
-app.use('/',wishlistRoute);
+const userRouter = require("./routes/userRouter");
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+app.use("/", userRouter);
+const adminRouter = require("./routes/adminRouter");
+app.use("/admin", adminRouter);
+const wishlistRoute = require("./routes/wishlistRouter");
+const { changePassword } = require("./controller/user_controller");
+app.use("/", wishlistRoute);
 
-app.get('*',(req, res) => {
-    try {
-        res.render('404')
-    } catch (error) {
-
-    }
-})
-  
+app.get("*", (req, res) => {
+  try {
+    res.render("404");
+  } catch (error) {}
+});
 
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-    console.log(`Server started running on http://localhost:${PORT}`);
+  console.log(`Server started running on http://localhost:${PORT}`);
 });
 
-const password=process.env.DB_PASS
+const password = process.env.DB_PASS;
 
-mongoose.connect(`mongodb+srv://shiblibasheer27:${password}@cluster0.iougxqn.mongodb.net/`); 
+mongoose.connect(
+  `mongodb+srv://shiblibasheer27:${password}@cluster0.iougxqn.mongodb.net/`
+);
